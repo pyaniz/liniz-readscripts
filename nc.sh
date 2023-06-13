76691769c5a097f8e3526746ec3e37bda9e35ab1
pyaniz <46324369+pyaniz@users.noreply.github.com>
2023-06-13 06:56:38 -0500

    Update nc.sh

--- nc.sh
+++ nc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt update && apt install wget sudo gnupg ffmpeg -y
+apt update && apt install wget sudo gnupg ffmpeg cmake imagemagick  -y
 echo "deb https://repo.delellis.com.ar bullseye bullseye" > /etc/apt/sources.list.d/20-pdlib.list
 wget -qO - https://repo.delellis.com.ar/repo.gpg.key | sudo apt-key add -
-apt-get update && apt-get install sudo php7.4-pdlib -y
+apt-get update && apt-get install sudo libbz2-dev liblapack-dev libopenblas-dev php7.4-pdlib -y
